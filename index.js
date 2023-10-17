68fc62811c95c41e423167236d20d156ca39dedd
Derek Harnett <derek.harnett@shaw.ca>
2023-10-17 09:15:08 -0700

    add hover states

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 $(document).ready(() => {
-    alert("Ready!")
+
+    // Add hover states to the social media icons
+    $(".flex-socials-list__icon-wrapper").hover(function () {
+        const iconWrapper = $(this)
+        const icon = iconWrapper.children(".flex-socials-list__icon")
+
+        iconWrapper.toggleClass("flex-socials-list__icon-wrapper--hover")
+        icon.toggleClass("flex-socials-list__icon--hover")
+    })
+
+    // Add hover state to the submit button
+    $(".flex-signup-form__submit-btn").hover(function () {
+        const button = $(this)
+
+        button.toggleClass("flex-signup-form__submit-btn--hover")
+    })
+
 })
