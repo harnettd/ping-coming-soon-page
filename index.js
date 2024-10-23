9a46d3f737fd2289ee5cc1d83e15ef2e1b568bbc
Derek Harnett <DHarnett.dev@proton.me>
2024-10-22 22:07:46 -0700

    add essential javascript functionality

--- index.js
+++ index.js
@@ -1,27 +1,44 @@
-$(document).ready(() => {
+const emailInput = document.querySelector(".flex-signup-form__email-input");
+const errMsg = document.querySelector(".flex-signup-form__err-msg");
+const submitBtn = document.querySelector(".flex-signup-form__submit-btn");
 
-    // Add hover states to the social media icons
-    $(".flex-socials-list__icon-wrapper").hover(function () {
-        const iconWrapper = $(this)
-        const icon = iconWrapper.children(".flex-socials-list__icon")
+// When the webpage loads, clear the value of emailInput and deactivate error messages.
+emailInput.value = "";
+emailInput.classList.remove("flex-signup-form__email-input--on-err");
+errMsg.classList.remove("flex-signup-form__err-msg--on-err");
 
-        iconWrapper.toggleClass("flex-socials-list__icon-wrapper--hover")
-        icon.toggleClass("flex-socials-list__icon--hover")
-    })
+const isValidEmail = (email) => {
+  return false;
+};
 
-    // Add hover state to the submit button
-    $(".flex-signup-form__submit-btn").hover(function () {
-        const button = $(this)
+// Add email validation to the submit button
+$(".flex-signup-form__submit-btn").click((evt) => {
+  evt.preventDefault();
+  console.log("Click!");
 
-        button.toggleClass("flex-signup-form__submit-btn--hover")
-    })
+  const input = document.querySelector(".flex-signup-form__email-input");
+  const email = input.value;
 
-    // Add hover state to the email input
-    $(".flex-signup-form__email-input").hover(function () {
-        const emailInput = $(this)
-        const errroMessage = emailInput.next(".flex-signup-form__err-msg")
+  console.log(input);
+  console.log(`Email address: ${email}`);
 
-        emailInput.toggleClass("flex-signup-form__email-input--on-err")
-        errroMessage.toggleClass("flex-signup-form__err-msg--on-err")
-    })
-})
+  if (!isValidEmail(email)) {
+    document
+      .querySelector(".flex-signup-form__email-input")
+      .classList.add("flex-signup-form__email-input--on-err");
+    document
+      .querySelector(".flex-signup-form__err-msg")
+      .classList.add("flex-signup-form__err-msg--on-err");
+  } else {
+    console.log("Valid email address");
+  }
+});
+
+// Add hover states to the social media icons
+$(".flex-socials-list__icon-wrapper").hover(function () {
+  const iconWrapper = $(this);
+  const icon = iconWrapper.children(".flex-socials-list__icon");
+
+  iconWrapper.toggleClass("flex-socials-list__icon-wrapper--hover");
+  icon.toggleClass("flex-socials-list__icon--hover");
+});
