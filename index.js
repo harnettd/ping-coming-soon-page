014e49bb2f83f8968458b80a608a05c93b73d973
Derek Harnett <DHarnett.dev@proton.me>
2024-10-28 21:57:56 -0700

    complete index.js

--- index.js
+++ index.js
@@ -1,35 +1,33 @@
-const emailInput = document.querySelector(".flex-signup-form__email-input");
-const errMsg = document.querySelector(".flex-signup-form__err-msg");
-const submitBtn = document.querySelector(".flex-signup-form__submit-btn");
+const formBlock = "flex-signup-form";
+const emailInput = document.querySelector(`.${formBlock}__email-input`);
+const errMsg = document.querySelector(`.${formBlock}__err-msg`);
+const submitBtn = document.querySelector(`.${formBlock}__submit-btn`);
 
-// When the webpage loads, clear the value of emailInput and deactivate error messages.
+// When the webpage loads, clear the value of emailInput and deactivate error
+// indicators.
 emailInput.value = "";
-emailInput.classList.remove("flex-signup-form__email-input--on-err");
-errMsg.classList.remove("flex-signup-form__err-msg--on-err");
+emailInput.classList.remove(`${formBlock}__email-input--on-err`);
+errMsg.classList.remove(`${formBlock}__err-msg--on-err`);
 
+// Return true if email is valid and false otherwise.
 const isValidEmail = (email) => {
-  return false;
+  const re = /^\w+(\.\w+)*@\w+(\.\w+)*$/;
+  return re.test(email);
 };
 
-// Add email validation to the submit button
-$(".flex-signup-form__submit-btn").click((evt) => {
+// When the Submit button is clicked, checker whether or not emailInput
+// contained a valid email address. Activate or deactivate error
+// indicators accordingly.
+submitBtn.addEventListener("click", function (evt) {
   evt.preventDefault();
-  console.log("Click!");
-
-  const input = document.querySelector(".flex-signup-form__email-input");
-  const email = input.value;
-
-  console.log(input);
-  console.log(`Email address: ${email}`);
-
-  if (!isValidEmail(email)) {
-    document
-      .querySelector(".flex-signup-form__email-input")
-      .classList.add("flex-signup-form__email-input--on-err");
-    document
-      .querySelector(".flex-signup-form__err-msg")
-      .classList.add("flex-signup-form__err-msg--on-err");
+  email = emailInput.value;
+  if (isValidEmail(email)) {
+    console.log(`${email} is a valid email address`);
+    emailInput.classList.remove(`${formBlock}__email-input--on-err`);
+    errMsg.classList.remove(`${formBlock}__err-msg--on-err`);
   } else {
-    console.log("Valid email address");
+    console.log(`${email} is not a valid email address`);
+    emailInput.classList.add(`${formBlock}__email-input--on-err`);
+    errMsg.classList.add(`${formBlock}__err-msg--on-err`);
   }
 });
