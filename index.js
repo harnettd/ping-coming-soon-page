cac53a8f509884ad9dc40f0f4ce2ece4bb0cf462
Derek Harnett <DHarnett.dev@proton.me>
2025-02-20 03:23:15 -0800

    update index.js to add particular error message for empty email

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const BEM = (block, element, modifier) => {
   const BE = `${block}__${element}`;
   return modifier ? `${BE}--${modifier}` : BE;
-}
+};
 
 // BEM class parts
 const formBlock = "flex-signup-form";
@@ -16,18 +16,19 @@ const input = document.querySelector("." + BEM(formBlock, inputElement));
 const errMsg = document.querySelector("." + BEM(formBlock, errMsgElement));
 
 // Add error indicators
-const addErr = () => {
-  form.classList.add(`${formBlock}--${errModifier}`)
+const addErr = (message) => {
+  form.classList.add(`${formBlock}--${errModifier}`);
   input.classList.add(BEM(formBlock, inputElement, errModifier));
   errMsg.classList.add(BEM(formBlock, errMsgElement, errModifier));
-}
+  errMsg.innerHTML = message;
+};
 
 // Remove error indicators
 const removeErr = () => {
-  form.classList.remove(`${formBlock}--${errModifier}`)
+  form.classList.remove(`${formBlock}--${errModifier}`);
   input.classList.remove(BEM(formBlock, inputElement, errModifier));
   errMsg.classList.remove(BEM(formBlock, errMsgElement, errModifier));
-}
+};
 
 // When the webpage loads, clear the value of input and remove error indicators.
 window.addEventListener("load", (evt) => {
@@ -38,7 +39,7 @@ window.addEventListener("load", (evt) => {
 // When input gets focus, remove error indicators.
 input.addEventListener("focus", (evt) => {
   removeErr();
-})
+});
 
 // Return true if email is valid and false otherwise.
 const isValidEmail = (email) => {
@@ -53,9 +54,13 @@ form.addEventListener("submit", (evt) => {
   email = input.value;
   if (isValidEmail(email)) {
     removeErr();
-    alert("Email address is valid")
+    alert("Email address is valid");
   } else {
-    addErr();
+    if (!email) {
+      addErr("Whoops! It looks like you forgot to add your email");
+    } else {
+      addErr("Please provide a valid email address");
+    }
     evt.preventDefault();
   }
 });
